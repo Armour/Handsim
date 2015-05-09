24688d2baaf889e4477b43677295d9287a3bf640
Armour <497052684@qq.com>
2015-05-10 00:42:34 +0800

    fixed j-type-error

--- assembler/dismips.py
+++ assembler/dismips.py
@@ -6,6 +6,12 @@ from disassembleDict import *
 
 command = []
 
+def handle(target, PC):
+    if target[0] == '0':
+        return PC + int(target, 2)
+    else:
+        return PC - int(bin(int(target[1:], 2) - 1).replace('1', '_').replace('0', '1').replace('_', '0')[2:], 2)
+
 def R_type(line):
     if R_dict[line[-6:]][0] == 0:
         return R_dict[line[-6:]][1] + ' ' + code[line[16:21]] + ', ' + code[line[6:11]] + ', ' + code[line[11:16]]
@@ -24,20 +30,20 @@ def R_type(line):
     elif R_dict[line[-6:]][0] == 7:
         return R_dict[line[-6:]][1]
 
-def I_type(line):
+def I_type(line, PC):
     if I_dict[line[0:6]][0] == 0:
         return I_dict[line[0:6]][1] + ' ' + code[line[11:16]] + ', ' + str(int(line[16:], 2)) + '(' + code[line[6:11]] + ')'
     elif I_dict[line[0:6]][0] == 1:
-        return I_dict[line[0:6]][1] + ' ' + code[line[6:11]] + ', ' + 'mark_' + str(int(line[16:], 2)/4)
+        return I_dict[line[0:6]][1] + ' ' + code[line[6:11]] + ', ' + 'mark_' + str(handle(line[16:], PC/4))
     elif I_dict[line[0:6]][0] == 2:
-        return I_dict[line[0:6]][1] + ' ' + code[line[6:11]] + ', ' + code[line[11:16]] + ', mark_' + str(int(line[16:], 2)/4)
+        return I_dict[line[0:6]][1] + ' ' + code[line[6:11]] + ', ' + code[line[11:16]] + ', mark_' + str(handle(line[16:], PC/4))
     elif I_dict[line[0:6]][0] == 3:
         return I_dict[line[0:6]][1] + ' ' + code[line[11:16]] + ', ' + code[line[6:11]] + ', ' + str(int(line[16:], 2))
     elif I_dict[line[0:6]][0] == 4:
         return I_dict[line[0:6]][1] + ' ' + code[line[11:16]] + ', ' + code[line[6:11]]
 
 def J_type(line):
-    return J_dict[line[0:6]] + ' mark_' + str(int(line[6:], 2)/4)
+    return J_dict[line[0:6]] + ' mark_' + str(int(line[6:], 2))
 
 def first_time_handle():
     PC = 0
@@ -47,7 +53,7 @@ def first_time_handle():
             if line[0:6] == '000000':
                 res = R_type(line)
             elif line[0:6] in I_dict:
-                res = I_type(line)
+                res = I_type(line, PC)
             elif line[0:6] in J_dict:
                 res = J_type(line)
             command.append(res)
